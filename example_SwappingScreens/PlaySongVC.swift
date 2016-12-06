678c54cf32eda9c0a4ff532f435b0b387339d3c3
Quang Nguyen <qilytquang@gmail.com>
2016-12-06 21:33:41 +0700

    first commit - completed SwappingScreens with segue example

---
+++ example_SwappingScreens/PlaySongVC.swift
@@ -0,0 +1,47 @@
+//
+//  PlaySongVC.swift
+//  example_SwappingScreens
+//
+//  Created by quang nguyen on 12/6/16.
+//  Copyright © 2016 quang nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PlaySongVC: UIViewController {
+
+    @IBOutlet weak var songTitleLbl: UILabel!
+    
+    private var _selectedSong: String!
+    
+    var selectedSong: String {
+        get {
+            return _selectedSong
+        } set {
+            _selectedSong = newValue
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        songTitleLbl.text = _selectedSong
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ example_SwappingScreens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example_SwappingScreens
+//
+//  Created by quang nguyen on 12/6/16.
+//  Copyright © 2016 quang nguyen. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
